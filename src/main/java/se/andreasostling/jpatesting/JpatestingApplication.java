d714e7e2099b80dff39ba59fe62ba0315689dd0d
Andreas Östling <ajlostling@gmail.com>
2018-09-22 19:40:25 +0200

    First commit

---
+++ src/main/java/se/andreasostling/jpatesting/JpatestingApplication.java
@@ -0,0 +1,12 @@
+package se.andreasostling.jpatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpatestingApplication.class, args);
+	}
+}
